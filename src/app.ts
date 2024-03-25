4b09b0eeb64dd3f5939fec64d15169f9da8aef91
pari.dev <pari.dev@gmail.com>
2024-03-26 00:05:44 +0330

    Some deletion

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import startBrowser from "./browser.ts";
-import scraperController from "./page.controller.ts";
-
-//Start the browser and create a browser instance
-let browserInstance = startBrowser();
-
-// Pass the browser instance to the scraper controller
-scraperController(browserInstance);
