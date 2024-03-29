c17945b16158926eeb45b4d92d69826232a4c057
pari.dev <pari.dev@gmail.com>
2024-03-29 22:42:32 +0330

    Refactored database

---
+++ src/database.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+export default function startDB() {
+  mongoose
+    .connect(process.env.MONGO!)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => console.error(err));
+}
