fec928d1ade31fdf59af96799bd19599e8dea05c
pari.dev <pari.dev@gmail.com>
2024-03-29 23:12:09 +0330

    Changed Mongoose model

--- model/info.schema.ts
+++ model/info.schema.ts
@@ -13,11 +13,20 @@ const infoSchema = new mongoose.Schema(
       required: [true, "The business must have a domain"],
       trim: true,
     },
+    province: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
     stars: {
       type: Number,
     },
+    certifiedDate: {
+      type: String,
+    },
     expirationDate: {
-      type: Date,
+      type: String,
     },
   },
   { timestamps: true }
